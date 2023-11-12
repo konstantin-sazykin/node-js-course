d258637b57280fbbcdfcdb3b2a201d07c4a8be42
Konstantin Sazykin <kvsazykin@gmail.com>
2023-11-12 13:31:10 +0300

    added env

--- src/settings.ts
+++ src/settings.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import { router } from './router';
+import dotenv from 'dotenv';
 
 export const app = express();
 
+dotenv.config()
+
+console.log(`${process.env.BASE_PATH}/api`);
+
+
 app.use(express.json());
-app.use('/api', router);
+app.use(`${process.env.BASE_PATH}/api`, router);
 
