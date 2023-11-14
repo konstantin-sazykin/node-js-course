d697958eaecdcd581cde56d2309c42bdcd2b54d0
Konstantin Sazykin <kvsazykin@gmail.com>
2023-11-14 16:52:35 +0300

    Moved types directory

---
+++ src/types/video/output.ts
@@ -0,0 +1,23 @@
+export type VideoType = {
+  id: number;
+  title: string;
+  author: string;
+  canBeDownloaded: boolean;
+  minAgeRestriction: number | null;
+  createdAt: string;
+  publicationDate: string;
+  availableResolutions: AvailableResolutionsType;
+};
+
+export enum ResolutionsEnum {
+  P144,
+  P240,
+  P360,
+  P480,
+  P720,
+  P1080,
+  P1440,
+  P2160,
+}
+
+export type AvailableResolutionsType = ResolutionsEnum | null | never[];
