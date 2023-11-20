f030197a6fbefa830a831156d152dce566218b4b
Konstantin Sazykin <kvsazykin@gmail.com>
2023-11-20 13:33:12 +0300

    Changed ResponseStatusCodesEnum location

--- src/types/common.ts
+++ src/types/common.ts
@@ -11,13 +11,3 @@ export interface ErrorType {
   errorsMessages: ErrorMessageType[];
 }
 
-export enum ResponseStatusCodesEnum {
-  Ok = 200,
-  Created = 201,
-  NoContent = 203,
-  BadRequest = 400,
-  Unathorized = 401,
-  Forbidden = 403,
-  NotFound = 404,
-  InternalError = 500,
-}
---
+++ src/utils/constants.ts
@@ -0,0 +1,17 @@
+export enum ResponseStatusCodesEnum {
+  Ok = 200,
+  Created = 201,
+  NoContent = 204,
+  BadRequest = 400,
+  Unathorized = 401,
+  Forbidden = 403,
+  NotFound = 404,
+  InternalError = 500,
+}
+
+export enum RoutesPathsEnum {
+  videos = '/videos',
+  blogs = '/blogs',
+  posts = '/posts',
+  testingAllData = '/testing/all-data',
+}
