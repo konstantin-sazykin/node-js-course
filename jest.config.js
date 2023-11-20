be2e658356cee04b3815968aad4be32a05ed1a87
Konstantin Sazykin <kvsazykin@gmail.com>
2023-11-20 17:19:13 +0300

    Removing code from paths alias

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,5 @@
-const tsconfig = require('./tsconfig.json');
-const moduleNameMapper = require('tsconfig-paths-jest')(tsconfig);
-
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testRegex: '.e2e.test.ts$',
-  moduleNameMapper
 };
