940b84fc72ac4969ff6edd67a3c92612373d037d
Konstantin Sazykin <kvsazykin@gmail.com>
2023-11-20 13:22:30 +0300

    Added ts-config-paths-jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
+const tsconfig = require('./tsconfig.json');
+const moduleNameMapper = require('tsconfig-paths-jest')(tsconfig);
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testRegex: '.e2e.test.ts$'
+  testRegex: '.e2e.test.ts$',
+  moduleNameMapper
 };
