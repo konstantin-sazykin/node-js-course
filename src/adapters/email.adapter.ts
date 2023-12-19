64ec286a25eb2ca6d606ea015ae6ab00a3c69355
Konstantin Sazykin <kvsazykin@gmail.com>
2023-12-19 17:08:53 +0300

    Added Email adapter

---
+++ src/adapters/email.adapter.ts
@@ -0,0 +1,40 @@
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const ADDRESS = process.env.EMAIL_ADDRESS;
+const PASSWORD = process.env.EMAIL_PASSWORD;
+
+export class EmailAdapter {
+  static async sendEmail(email: string, subject: string, message: string) {
+    try {
+      if (!ADDRESS || !PASSWORD) {
+        throw new Error('The username or password is not available');
+      }
+
+      const transport = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: ADDRESS,
+          pass: PASSWORD,
+        },
+      });
+
+      const info = await transport.sendMail({
+        from: `Konstantin <${ADDRESS}>`,
+        to: email,
+        subject: subject,
+        html: message,
+      });
+
+      const isAccepted = info.accepted.includes(email);
+      
+      return isAccepted;
+    } catch (error) {
+      console.error(error);
+
+      return false;
+    }
+  }
+}
