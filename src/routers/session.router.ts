28583b6bf327cf6b93ae75c6a972f7170ca5ed87
Konstantin Sazykin <kvsazykin@gmail.com>
2024-01-14 17:05:03 +0300

    Added deleteDeviceByIdValidation

--- src/routers/session.router.ts
+++ src/routers/session.router.ts
@@ -3,9 +3,10 @@ import { Router } from 'express';
 
 import { SessionController } from '../conrollers/session.controller';
 import { refreshTokenMiddleware } from '../middlewares/refreshToken/refreshToken.middleware';
+import { deleteDeviceByIdValidation } from '../validators/device.validator';
 
 export const sessionRouter = Router();
 
 sessionRouter.get('/', refreshTokenMiddleware, SessionController.getAllSessions);
 sessionRouter.delete('/', refreshTokenMiddleware, SessionController.removeAllSessions);
-sessionRouter.delete('/:id', refreshTokenMiddleware, SessionController.removeById);
+sessionRouter.delete('/:id', refreshTokenMiddleware, deleteDeviceByIdValidation, SessionController.removeById);
---
+++ src/validators/device.validator.ts
@@ -0,0 +1,39 @@
+import { type NextFunction, type Request, type Response } from 'express';
+import { type ValidationChain, param, validationResult } from 'express-validator';
+
+import { ResponseStatusCodesEnum } from '../utils/constants';
+import { ApiError } from '../exeptions/api.error';
+import { SessionQueryRepository } from '../repositories/session/session.query-repository';
+
+const deviceIdParamValidation = param('id').custom(async (deviceId: string, { req }) => {
+  const userId: string = req.userId;
+
+  const session = await SessionQueryRepository.find(deviceId);
+
+  if (!session) {
+    throw new Error(`Device с id ${deviceId} не найден`);
+  };
+
+  console.log({ userId, sessUsId: session.userId });
+
+  if (session.userId !== userId) {
+    throw new Error('Нет прав для удаления');
+  }
+});
+
+export const requestParamsValidation = (request: Request, response: Response, next: NextFunction): void => {
+  const errors = validationResult(request);
+  if (!errors.isEmpty()) {
+    next(new ApiError(ResponseStatusCodesEnum.Forbidden, null)); return;
+  }
+
+  next();
+};
+
+export const deleteDeviceByIdValidation = (): [
+  ValidationChain,
+  (request: Request, response: Response, next: NextFunction) => void,
+] => [
+  deviceIdParamValidation,
+  requestParamsValidation,
+];
