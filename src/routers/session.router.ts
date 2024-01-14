629cf99c435449a5773568f6c90bb6a45b318a4c
Konstantin Sazykin <kvsazykin@gmail.com>
2024-01-14 16:39:17 +0300

    Added auth mw to session controller

--- src/routers/session.router.ts
+++ src/routers/session.router.ts
@@ -1,9 +1,11 @@
+// @ts-nocheck
 import { Router } from 'express';
 
 import { SessionController } from '../conrollers/session.controller';
+import { authMiddleware } from '../middlewares/auth/auth.middleware';
 
 export const sessionRouter = Router();
 
 sessionRouter.get('/', SessionController.getAllSessions);
-sessionRouter.delete('/', SessionController.removeAllSessions);
-sessionRouter.delete('/:id', SessionController.removeById);
+sessionRouter.delete('/', authMiddleware, SessionController.removeAllSessions);
+sessionRouter.delete('/:id', authMiddleware, SessionController.removeById);
