76204441e79a8f025d678c2e31f2c537d9e82fc6
Konstantin Sazykin <kvsazykin@gmail.com>
2024-01-18 00:16:07 +0300

    Mock console

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,3 +1,6 @@
 const { RateLimitService } = require("../src/domain/rateLimit.service");
 
 RateLimitService.checkLimit = jest.fn(() => Promise.resolve('It is from mocked function'));
+
+console.info = jest.fn();
+console.error = jest.fn();
